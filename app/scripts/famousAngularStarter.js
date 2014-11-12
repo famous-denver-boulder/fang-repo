a9865c1456ed121cb5ed8e0a4c35c99d9e4adff0
Lee <leeblazek@gmail.com>
2014-11-11 23:11:02 -0700

    init

---
+++ app/scripts/famousAngularStarter.js
@@ -0,0 +1,22 @@
+'use strict';
+
+angular.module('famousAngularStarter', ['ngAnimate', 'ngCookies',
+  'ngTouch', 'ngSanitize',
+  'ngResource', 'ui.router',
+  'famous.angular'
+])
+  .config(function($stateProvider, $urlRouterProvider) {
+    $stateProvider
+      .state('home', {
+        url: '/',
+        templateUrl: 'partials/main.html',
+        controller: 'MainCtrl'
+      })
+      .state('jade', {
+        url: '/jade',
+        templateUrl: 'partials/jade.html',
+        controller: 'MainCtrl'
+      });
+
+    $urlRouterProvider.otherwise('/');
+  });
---
+++ gulp/wiredep.js
@@ -0,0 +1,26 @@
+'use strict';
+
+var gulp = require('gulp');
+
+var $ = require('gulp-load-plugins')();
+
+// inject bower components
+gulp.task('wiredep', function() {
+  console.log('running wiredep');
+  var wiredep = require('wiredep').stream;
+
+  // gulp.src('app/styles/*.scss')
+  //   .pipe(wiredep({
+  //       directory: 'app/bower_components'
+  //   }))
+  //   .pipe(gulp.dest('app/styles'));
+
+  gulp.src('app/*.html')
+    .pipe(wiredep({
+      directory: 'app/bower_components',
+      exclude: ['bootstrap-sass-official']
+    }))
+    .pipe(gulp.dest('app'))
+    .pipe(gulp.dest('dist'));
+
+});
